e1e5bd3937f8b291a86d1bb0e17ea8646ba1e718
require64bits <tankoaraujo@gmail.com>
2020-06-25 22:42:01 -0300

    União dos módulos em um arquivo só!

--- Controle de estoque/python/main.py
+++ Controle de estoque/python/main.py
@@ -1,17 +1,17 @@
 #Importações Necessárias
-from flask import Flask, request, render_template, Blueprint
+from flask import Flask, request, render_template
 import gspread
-from Modules.delProdutos import Remover_Produto
-from Modules.retirarQuantProdutos import Remover_Item
 
 #Conexão com a Planilha
 conexao = gspread.service_account()
 planilha = conexao.open("Nature Saboaria").sheet1
 
 #Aplicação:
+#A variável root_path você deve modificar com o caminho completo da pasta python no seu sistema, serve para o Flask achar a pasta templates corretamente ^^
 app = Flask("Estoque-SIM-SA", root_path="c:\\Users\\tanko\\estoque-sim-sa\\Controle de estoque\\python\\")
 @app.route("/")
 def main():
+    #Isso aqui é era só pra mostrar o poder do Jinja2, pode remover se quiser
     #Obtendo a planilha completa sem os valores "Nome", "Quantidade" e etc
     planilha_completa = []
     for produto in planilha.get_all_values():
@@ -19,26 +19,39 @@ def main():
             continue
         planilha_completa.append(produto)
     #Gerando um hmtl com todos os produtos da planilha com o Jinja2, dê uma olhada no arquivo listarProdutos e base na pasta templates
-    return render_template("listarProdutos.html", planilha_completa = planilha_completa)
+    return render_template("remover_quantidade.html")
 
 #Roteamento para remover um produto
-@app.route("/remover")
+@app.route("/remover", methods=["POST"])
 def remove():
-    if Remover_Produto(planilha, request.args.get("nome")) == "S":
-        return "Feito!"
-    elif Remover_Produto(planilha, request.args.get("nome")) == "P":
-        return "Houve um erro na pesquisa do produto! Confira se digitou corretamente"
+    #Pesquisa o nome enviado na planilha
+    remover = planilha.find(request.form.get("nome"))
+    #Verifica se encontrou o produto
+    if not remover:
+        return u"""<script>alert("Houve um erro na pesquisa do produto! Confira se digitou corretamente.")</script>"""
+    #Faz a remoção do produto e avalia se a exclusão foi bem sucedida ou não
+    if planilha.delete_rows(remover.row):
+        return u"""<script>alert("Feito!")</script>"""
     else:
-        return "Houve um Erro ao deletar o produto!"
+        return u"""<script>alert("Houve um Erro ao deletar o produto!")</script>"""
 
 #Roteamento para remover uma quantidade de um produto, caso a quantidade do produto fique abaixo do limite, ele dispara um alerta
-@app.route("/remover_qtd")
+@app.route("/remover_qtd", methods=["POST"])
 def retirar():
-    if Remover_Item(planilha, request.args.get("nome"), int(request.args.get("quantidade")), 5) == "S":
-        return "Operação feita com sucesso!"
-    elif Remover_Item(planilha, request.args.get("nome"), int(request.args.get("quantidade")), 5) == "L":
-        return "Atenção! O produto está abaixo do limite especificado"  
+    #Procura o Produto
+    rm = planilha.find(request.form.get("nome"))
+    #Verifica se encontrou o produto
+    if not rm:
+        return u"""<script>alert("Houve um erro na pesquisa do produto! Confira se digitou corretamente.")</script>"""
+    #Verifica se a quantidade que vai ser retirada é maior que a quantidade disponível, se sim, retorna um erro
+    if int(planilha.cell(rm.row, 2).value) < int(request.form.get("quantidade")):
+        return u"""<script>alert("A quantidade que você quer retirar é maior que a quantidade disponível!Tente colocar um número menor!")</script>"""
+    #Atualiza a célula com o valor da subtração do valor que já tem na célula com o valor que o usuário quer retirar
+    planilha.update_cell(rm.row, 2, int(planilha.cell(rm.row, 2).value) - int(request.form.get("quantidade")))
+    #Verifica se a quantidade atual está abaixo do valor limite definido pelo usuário (por enquanto o limite é fixo kkkkk)
+    if int(planilha.cell(rm.row, 2).value) < 5:
+        return u"""<script>alert("Atenção! O produto está abaixo do limite especificado")</script>"""
     else:
-        return "A quantidade que você quer retirar é maior que a quantidade disponível!Tente colocar um número menor!"
+        return u"""<script>alert("Operação feita com sucesso!")</script>"""
 
 app.run(debug=True, use_reloader=True)
