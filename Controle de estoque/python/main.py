e0cda7cb07cbec4978a9cd8fa2456cfaf2562048
require64bits <tankoaraujo@gmail.com>
2020-07-02 19:38:47 -0300

    Correção retorno do botão de Remover

--- Controle de estoque/python/main.py
+++ Controle de estoque/python/main.py
@@ -9,29 +9,36 @@ planilha = conexao.open("Nature Saboaria").sheet1
 
 #Aplicação:
 #A variável root_path você deve modificar com o caminho completo da pasta python no seu sistema, serve para o Flask achar a pasta templates corretamente ^^
-app = Flask("Estoque-SIM-SA", root_path="/home/lucas/Desktop/estoque-sim-sa/Controle de estoque/python")
-#app = Flask("Estoque-SIM-SA",  root_path="C:\\Users\\tanko\\estoque-sim-sa\\Controle de estoque\\python")
+#app = Flask("Estoque-SIM-SA", root_path="/home/lucas/Desktop/estoque-sim-sa/Controle de estoque/python")
+app = Flask("Estoque-SIM-SA",  root_path="C:\\Users\\tanko\\estoque-sim-sa\\Controle de estoque\\python")
 
 
 @app.route("/")
 def main():
     return render_template("incluirProduto.html", planilha_completa = planilha.get_all_values())
 
+@app.route("/delete")
+def delete():
+    if request.form.get("delete"):
+        return request.form.get("delete")
+    if request.form.get("venda"):
+        return request.form.get("venda")
+
 #Roteamento para remover um produto
 @app.route("/remover", methods=["POST"])
 def remove():
     #Pesquisa o nome enviado na planilha
-    remover = planilha.find(request.form.get("nome"))
+    remover = planilha.find(request.form.get("delete"))
 
     #Verifica se encontrou o produto
     if not remover:
-        return render_template("resposta.html", retorno = "Houve um erro na pesquisa do produto! Confira se digitou corretamente.")
+        return render_template("respostaEstoque.html", retorno = "Houve um erro na pesquisa do produto! Confira se digitou corretamente.")
 
     #Faz a remoção do produto e avalia se a exclusão foi bem sucedida ou não
     if planilha.delete_rows(remover.row):
-        return render_template("resposta.html", retorno = "Feito!")
+        return render_template("respostaEstoque.html", retorno = "Feito!")
     else:
-        return render_template("resposta.html", retorno = "Houve um Erro ao deletar o produto!")
+        return render_template("respostaEstoque.html", retorno = "Houve um Erro ao deletar o produto!")
 
 #Roteamento para remover uma quantidade de um produto, caso a quantidade do produto fique abaixo do limite, ele dispara um alerta
 @app.route("/remover_qtd", methods=["POST"])
